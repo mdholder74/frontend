f2a7a5bf8a6fe1281de53e0edc7f272fdd598053
mdholder74 <mdholder74@gmail.com>
2025-01-30 09:46:04 -0500

    created data for the NFL Analyst component

--- react-nfl-trivia-app/src/App.jsx
+++ react-nfl-trivia-app/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import Card from './components/Card'
 import Navigation from './components/Navigation'
+import Analyst from './components/Analyst'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Navigation />
       <Header />
       <Card />
+      <Analyst />
       <Footer />
     </main>
 
--- react-nfl-trivia-app/src/components/Analyst.jsx
+++ react-nfl-trivia-app/src/components/Analyst.jsx
@@ -1,7 +1,68 @@
 import React from 'react'
 
-export default function createAnalyst() {
+export default function Analyst() {
   return (
-    <div>createAnalyst</div>
+    <div>
+ <div class="analyst-container">
+        <h2>Create an NFL Analyst</h2>
+        <div class="form">
+            <label>Name:</label>
+            <select id="role">
+                <option>The Scoop Master</option>
+                <option>The Comeback King</option>
+                <option>The Playbook Professor</option>
+                <option>The Scheme King</option>
+                <option>The Hype Machine</option>
+                <option>The Monday Morning Coach</option>
+            </select>
+        </div>
+        <div class="form">
+            <label>Personality Traits:</label>
+            <select id="traits">
+                <option>Statistician</option>
+                <option>Passionate</option>
+                <option>Articulate</option>
+                <option>Confident</option>
+                <option>Charismatic</option>
+                <option>Annoying</option>
+            </select>
+        </div>
+        <div class="form">
+            <label>Clothing Style:</label>
+            <select id="clothing">
+                <option>The Classic Broadcaster (Suit and Tie)</option>
+                <option>The Former Player (Button-up, Jeans, Sneakers)</option>
+                <option>The Trendsetter (Designer Jackets, Tailored Pants, Oxfords)</option>
+                <option>The Tank Top (Black Tank Top, Rolex, Gold Chain, Blue Jeans, Sneakers)</option>
+            </select>
+        </div>
+        <div class="form">
+            <label>NFL Knowledge:</label>
+            <select id="expertise">
+                <option>Reporter</option>
+                <option>Former Player</option>
+                <option>Former Coach</option>
+                <option>I Love Football Guy</option>
+            </select>
+        </div>
+        <div class="form">
+            <label>Favorite Teams:</label>
+            <select id="teams">
+                <option>Dallas Cowboys</option>
+                <option>New England Patriots</option>
+                <option>Philadelphia Eagles</option>
+                <option>Pittsburgh Steelers</option>
+                <option>Kansas City Chiefs</option>
+                <option>Cincinnati Bengals</option>
+                <option>San Francisco 49ers</option>
+                <option>Green Bay Packers</option>
+                <option>Buffalo Bills</option>
+                <option>Baltimore Ravens</option>
+            </select>
+        </div>
+        <button onclick="saveAnalyst()">Save Analyst</button>
+    </div>
+
+    </div>
   )
 }
